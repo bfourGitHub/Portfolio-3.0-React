af83491f1859bbb4b2c2a2c910ca3b3079fc5d6c
Bryan Moreno <bfourbryan@gmail.com>
2021-03-12 00:24:37 -0800

    Addition of assets folder with photos. Conversion of a tags to jsx with working external links. Install bootstrap and minor css changes.

--- portfolio-3.0-react/src/App.js
+++ portfolio-3.0-react/src/App.js
@@ -1,10 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 // import logo from './logo.svg';
 import './App.css';
+
+import adventue from './assets/adventue.jpg';
+import coding from './assets/coding.jpg';
+import DailyPlannerSS from './assets/DailyPlannerSS.png';
+import FoodDadABaseSS from './assets/FoodDadABaseSS.png';
+import HeroPhotoNEW from './assets/HeroPhotoNEW.png';
+import macbook from './assets/macbook.jpg';
+import typewriter from './assets/typewriter.jpg';
+
 
 
 
 function App() {
   return (
+    
     <div className="App">
       
       <nav className="sticky-top">
@@ -24,7 +35,7 @@ function App() {
         </div>
       </nav>
       <br />
-      <img src="./assets/heroSelfie.png" className="img-fluid col-12" alt="..." />
+      <img src={HeroPhotoNEW} className="img-fluid col-12" alt="..." />
 
       <div className="cardCluster" id="aboutMe">
 
@@ -34,87 +45,87 @@ function App() {
             <h6 className="card-subtitle mb-2 text-muted">full stack developer</h6>
             <p className="card-text">Student. UW Coding Bootcamp 2021. Bartender with a love of coding & all things
             tech.</p>
-            <link href="https://linkedin.com/in/bryan-moreno-360877128" alt="LinkedIn Profile" className="card-link fab fa-linkedin"/>
-            <link href="https://drive.google.com/file/d/12-DtfiCD9neXaYTvRGocvRiiZqTtc2_0/view?usp=sharing"
-              className="card-link fas fa-portrait" alt="Resume PDF"/>
-            <link href="https://github.com/bfourGitHub" className="card-link fab fa-github-square"
-              alt="GitHub Profile"/>
-
+            
+            <a href="https://linkedin.com/in/bryan-moreno-360877128" className="card-link fab fa-linkedin"/>
+            
+            <a href="https://drive.google.com/file/d/12-DtfiCD9neXaYTvRGocvRiiZqTtc2_0/view?usp=sharing" className="card-link fas fa-portrait"/>
+            
+            <a href="https://github.com/bfourGitHub" className="card-link fab fa-github"/>
           </div>
         </div>
 
-        <div className="card" id="featuredProjects">
+        <div className="card featuredProjects" id="featuredProjects">
           <div className="card-body">
-            <img src="./assets/adventue.jpg" className="card-img-top" alt="..."/>
+            <img src={adventue} className="card-img-top" alt="..."/>
               <h5 className="card-title">Trail Chasers</h5>
               <p className="card-text">Your new hiking buddy! An easy to use application that helps you find your next
             hike.</p>
           </div>
 
             <div className="card-body">
-              <link href="https://github.com/TaaniBravo/Trail-Chaser-Hiking-App" className="card-link fab fa-github"/>
-              <link href="https://taanibravo.github.io/Trail-Chaser-Hiking-App/"
+              <a href="https://github.com/TaaniBravo/Trail-Chaser-Hiking-App" className="card-link fab fa-github"/>
+              <a href="https://taanibravo.github.io/Trail-Chaser-Hiking-App/"
                 className="card-link fas fa-external-link-alt"/>
             </div>
         </div>
 
-          <div className="card" id="featuredProjects">
+          <div className="card featuredProjects">
             <div className="card-body">
-              <img src="./assets/FoodDadABase SS.png" className="card-img-top" alt="..."/>
+              <img src={FoodDadABaseSS} className="card-img-top" alt="..."/>
                 <h5 className="card-title">Food Dad-A-Base</h5>
                 <p className="card-text">Easy to use app to test your cooking skills (no studying required) while also
             having a laugh!</p>
             </div>
 
               <div className="card-body">
-                <link href="https://github.com/goantonioUW/food-dadabase" className="card-link fab fa-github"/>
-                <link href="https://food-dad-a-base.herokuapp.com/login" className="card-link fas fa-external-link-alt"/>
+                <a href="https://github.com/goantonioUW/food-dadabase" className="card-link fab fa-github"/>
+                <a href="https://food-dad-a-base.herokuapp.com/login" className="card-link fas fa-external-link-alt"/>
               </div>
           </div>
 
-            <div className="card" id="featuredProjects">
+            <div className="card featuredProjects">
               <div className="card-body">
-                <img src="./assets/macbook-2617705_1920.jpg" className="card-img-top" alt="..."/>
+                <img src={macbook} className="card-img-top" alt="..."/>
                   <h5 className="card-title">ReadME Generator</h5>
                   <p className="card-text">A user friendly CLI that creates a readME.md file.</p>
               </div>
 
                 <div className="card-body">
-                  <link href="https://github.com/bfourGitHub/Easy-Readme-Generator" className="card-link fab fa-github"/>
+                  <a href="https://github.com/bfourGitHub/Easy-Readme-Generator" className="card-link fab fa-github"/>
                   
                 </div>
             </div>
 
-              <div className="card" id="featuredProjects">
+              <div className="card featuredProjects">
                 <div className="card-body">
-                  <img src="./assets/coding-924920_1920.jpg" className="card-img-top" alt="..."/>
+                  <img src={coding} className="card-img-top" alt="..."/>
                     <h5 className="card-title">Team Generator CLI</h5>
                     <p className="card-text">A simple CLI to create & manage your team.</p>
                 </div>
 
                   <div className="card-body">
-                    <link href="https://github.com/bfourGitHub/10-HW-Template-Engine-Employee-Summary"
+                    <a href="https://github.com/bfourGitHub/10-HW-Template-Engine-Employee-Summary"
                       className="card-link fab fa-github"/>
                     
                   </div>
               </div>
 
-                <div className="card" id="featuredProjects">
+                <div className="card featuredProjects">
                   <div className="card-body">
-                    <img src="./assets/Daily Planner SS.png" className="card-img-top" alt="..."/>
+                    <img src={DailyPlannerSS} className="card-img-top" alt="..."/>
                       <h5 className="card-title">Daily Planner</h5>
                       <p className="card-text">A simple calendar application to keep you on track throughout your day.</p>
                   </div>
 
                     <div className="card-body">
-                      <link href="https://github.com/bfourGitHub/Daily-Planner" className="card-link fab fa-github"/>
-                      <link href="https://bfourgithub.github.io/Daily-Planner/" className="card-link fas fa-external-link-alt"/>
+                      <a href="https://github.com/bfourGitHub/Daily-Planner" className="card-link fab fa-github"/>
+                      <a href="https://bfourgithub.github.io/Daily-Planner/" className="card-link fas fa-external-link-alt"/>
                     </div>
                 </div>
 
-                  <div className="card" id="featuredProjects">
+                  <div className="card featuredProjects">
                     <div className="card-body">
-                      <img src="./assets/typewriter.jpg" className="card-img-top" alt="..."/>
+                      <img src={typewriter} className="card-img-top" alt="..."/>
                         <h5 className="card-title">Andy Meets the Neighbors</h5>
                         <p className="card-text">A short story about an unassuming old building, its eccentric residents & the new
                           guy in #304.</p>
