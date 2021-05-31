1963e91344b74d632e55e9b0a463bbbdce5ab948
Bryan Moreno <bfourbryan@gmail.com>
2021-05-31 00:07:02 -0700

    Additions to skillsCard, added icons for coding languages, added image files for adobe programs. Further styling of skill card.

--- client/src/components/SkillsCard.js
+++ client/src/components/SkillsCard.js
@@ -5,7 +5,14 @@ function SkillsCard() {
     return (
 
         <div className="card col-11 skillsCard">
-            <p>Skills here. Icons.</p>
+            <div style={{ marginTop:"1rem" }} >
+            <h6 className="card-subtitle mb-3 mt-2 text-muted">programs & languages</h6>
+            <p className="card-link fab fa-html5"></p>
+            {/* <img src={HeroPhotoNEWMin} className="img-fluid" alt="..." /> */}
+            <p className="card-link fab fa-css3-alt"></p>
+            <p className="card-link fab fa-js-square"></p>
+            <p className="card-link fab fa-react"></p>
+            </div>
         </div>
 
     );
